015e31aa4d8dadce6cc06a14e16d0272ecf339cd
kain-C- <92867625+kain-C@users.noreply.github.com>
2023-10-26 16:52:08 -0300

    ma hello word

--- app/src/main/java/com/example/semana10_git/MainActivity.java
+++ app/src/main/java/com/example/semana10_git/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.print("esto es un cambio-hello word");
+        System.out.print("esto es un cambio-hello word--hola mundo ");
+        System.out.print("esto es un cambio-hello word--hola mundo ");
+        System.out.print("esto es un cambio-hello word--hola mundo ");
+        System.out.print("esto es un cambio-hello word--hola mundo ");
     }
 }
